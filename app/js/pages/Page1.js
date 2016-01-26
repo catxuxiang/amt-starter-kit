35397c79147300ee802465aa387c259f9938b9bf
Kross <2813402@qq.com>
2016-01-26 16:25:14 +0800

    123

--- app/js/pages/Page1.js
+++ app/js/pages/Page1.js
@@ -5,12 +5,48 @@ import {
 } from 'amazeui-touch';
 
 const Page1 = React.createClass({
+  getInitialState: function() {
+    return {
+      resultId: '111',
+      resultComment: '222'
+    };
+  },
+
+  componentDidMount: function() {
+        this.setState({
+	  resultId: "",
+          resultComment: ""
+        });
+
+	var nocache = require('superagent-no-cache');
+	var request = require('superagent');
+	
+	request
+	.get("http://192.168.0.105:6699/mysql")
+	.use(nocache)
+	.withCredentials()
+	.end(function(err, res){
+	      if (err != null) {
+		alert(err);
+	      } else {
+		      if (this.isMounted()) {
+			var result = JSON.parse(res.text);
+		        var lastGist = result[1];
+			this.setState({
+			  resultId: lastGist.id,
+			  resultComment: lastGist.comment
+			});
+		      }
+	      }
+	}.bind(this));
+  },  
+
   render() {
     return (
       <Container {...this.props}>
         <Group>
-          <h2>Page 1</h2>
-          <p>页面内容</p>
+          <h2>{ this.state.resultId }</h2>
+          <p>{ this.state.resultComment }</p>
         </Group>
       </Container>
     );
